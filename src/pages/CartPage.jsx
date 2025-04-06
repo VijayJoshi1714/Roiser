21ff4d910e8de50baf08988d729a0f0db95ce0e8
Vijay Joshi <bhushanjoshi1714@gmail.com>
2025-04-06 11:51:45 +0530

    first commit

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -2,7 +2,6 @@ import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { FaShoppingCart, FaTruck, FaCreditCard, FaHeadset } from 'react-icons/fa';
-import WOW from 'wowjs';
 
 const CartPage = () => {
     const dispatch = useDispatch();
